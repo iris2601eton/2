53dea2e1b64ccf7df4fa48ad30459d6d5160dbf2
Iris <iris@Irisde-Mac.local>
2018-10-09 23:31:46 -0700

    Initial Commit

---
+++ SteinsGate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SteinsGate
+//
+//  Created by Iris on 2018/10/9.
+//  Copyright © 2018 Iris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
